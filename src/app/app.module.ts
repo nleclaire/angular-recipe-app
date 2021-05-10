551451e783ac0effd80e6288ddcf28e5da7fe75f
nleclaire <noah.leclaire@gmail.com>
2021-05-10 12:17:38 -0400

    Adding components Categories, Login, Signup, and creating user service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    CategoriesComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {UserService} from "../services/user/user.service";
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+  public userName: string;
+  public emailAddress: string;
+  public password: string;
+
+  registerUser(): void {
+    const newUser = {userName: this.userName, emailAddress: this.emailAddress, password: this.password};
+    this.userService.registerUser(newUser);
+  }
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+  }
+
+}

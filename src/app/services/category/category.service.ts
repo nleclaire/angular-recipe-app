36b84bcd57542a5e22de060b5a9d12fd398f6198
nleclaire <noah.leclaire@gmail.com>
2021-05-10 16:53:56 -0400

    Adding logic for Categories

--- src/app/services/category/category.service.ts
+++ src/app/services/category/category.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+
+const herokuUrl = 'https://damp-bayou-38809.herokuapp.com';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CategoryService {
 
-  constructor() { }
+export class CategoryService {
+  constructor(private http: HttpClient) { }
+
+  getCategories(): any {
+    const token = localStorage.getItem('token');
+    const requestOptions = {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${token}`
+      }),
+    };
+    return this.http
+      .get(`${herokuUrl}/api/categories`, requestOptions);
+  }
+
+  createCategory(newCategory): any {
+    console.log(newCategory);
+    const token = localStorage.getItem('token');
+    const requestOptions = {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${token}`
+      }),
+    };
+    return this.http
+      .post(`${herokuUrl}/api/categories/`, newCategory, requestOptions);
+  }
+
 }
